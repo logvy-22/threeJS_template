939b6e7f5eda1efe62316b7162bf25cd725d8fb2
Victoria Sokolova <lynan7@gmail.com>
2019-02-01 16:57:26 +0200

    Initial setup

--- src/listeners.js
+++
@@ -1,3 +0,0 @@
-export const frontButton = document.querySelector('#front');
-export const topButton = document.querySelector('#top');
-export const perspectiveButton = document.querySelector('#perspective');
